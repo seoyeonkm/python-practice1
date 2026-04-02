c2c9e9654580448e24cbcce3aed92fcb22ec78ff
seoyeonkm <seoyon.kim2004@gmail.com>
2026-04-02 16:00:21 +0900

    practice4

--- 구구단.py
+++ 구구단.py
@@ -1,4 +1,11 @@
 for i in range (2, 10):     
-    print("#  %d 단 #" %(i))        
-    for r in range (1, 10): 
-        print("%d X %d = %d" %(i, r, i * r))
+    print("#  %d 단 #" %i, end="\t") 
+print()   
+
+
+for num1 in range (1, 10): 
+    for num2 in range(2,10):
+        print("%d X %d = %d" %(num2, num1, num2 * num1), end = "\t")
+    print()
+
+
